77e83513d87e1e2f86605ac3ede25855f129c27c
Armemius <rusgy@yandex.ru>
2026-05-10 21:53:46 +0300

    feat(task-1): add executable for task
    Provide executable for the task with input parsing and validations

--- task-1/app/task.cpp
+++ task-1/app/task.cpp
@@ -1,5 +1,43 @@
+#include <cstdlib>
+#include <exception>
+#include <fstream>
 #include <iostream>
+#include <memory>
 
-int main() {
-    std::cout << "Nothing here yet :)\n";
+#include "bot/bot_base.hpp"
+#include "bot/original_bot.hpp"
+#include "core/game.hpp"
+#include "parser/parser.hpp"
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: task <input>\n";
+        return EXIT_FAILURE;
+    }
+
+    std::ifstream in{argv[1]};
+    if (!in) {
+        std::cerr << "Unable to open input file: " << argv[1] << "\n";
+        return EXIT_FAILURE;
+    }
+
+    std::ofstream out{"results.txt"};
+    if (!out) {
+        std::cerr << "Unable to open output file: results.txt\n";
+        return EXIT_FAILURE;
+    }
+
+    try {
+        auto game = std::make_shared<tvb::core::Game>(tvb::parser::parse(in));
+        std::unique_ptr<tvb::bot::BotBase> bot = std::make_unique<tvb::bot::OriginalBot>(game, out);
+        bot->run();
+    } catch (const tvb::parser::ParserError &ex) {
+        out << ex.what() << "\n";
+        return EXIT_FAILURE;
+    } catch (const std::exception &ex) {
+        std::cerr << "Error: " << ex.what() << "\n";
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
