415495cb23153835acc42ddcdd1bbcaf6c680465
Armemius <rusgy@yandex.ru>
2026-05-13 22:39:59 +0300

    feat(task-2): add executable
    Add executable file for the task, so it would be possible to
    actually run the simulation :)

--- task-2/app/task.cpp
+++ task-2/app/task.cpp
@@ -1,5 +1,36 @@
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
 
-int main() {
-    std::cout << "Nothing here yet :)\n";
+#include "parser/parser.hpp"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: task <input>\n";
+        return EXIT_FAILURE;
+    }
+
+    std::ifstream in(argv[1]);
+    if (!in) {
+        std::cerr << "Unable to open input file: " << argv[1] << "\n";
+        return EXIT_FAILURE;
+    }
+
+    std::ofstream out{"result.txt"};
+    if (!out) {
+        std::cerr << "Unable to open output file: result.txt\n";
+        return EXIT_FAILURE;
+    }
+
+    try {
+        parser::parse(in, out).run();
+    } catch (const parser::InvalidInputLine &error) {
+        out << error.line() << "\n";
+        return EXIT_FAILURE;
+    } catch (const std::exception &ex) {
+        std::cerr << "Error: " << ex.what() << "\n";
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
