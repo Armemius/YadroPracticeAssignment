3fff5e3a647c50cb1afc71ce2ce1413e4e86e864
Armemius <rusgy@yandex.ru>
2026-05-13 15:09:08 +0300

    feat(task-2): add domain-specific types
    Add types to represent different entities of the simulation depending
    of their max sizes

---
+++ task-2/include/simulation/types.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cstdint>
+
+namespace sim {
+
+/// Index of the product type
+using product_type_t = uint8_t;
+
+/// Index of the product
+using product_index_t = uint32_t;
+
+/// Struct representing state for certain product
+struct Product {
+    product_index_t index;
+    product_type_t type;
+};
+
+/// Product state
+using product_t = Product;
+
+/// Index of the operation type
+using operation_t = uint8_t;
+
+/// Index of the machine
+using machine_t = uint8_t;
+
+/// Operation time
+using optime_t = uint16_t;
+
+/// Time point of simulation
+using simtime_t = uint64_t;
+
+/**
+     * @brief Upgrades product
+     * 
+     * @param product Product to upgrade
+     */
+inline void upgrade_product(product_t &product) {
+    ++product.type;
+}
+
+}  // namespace sim
